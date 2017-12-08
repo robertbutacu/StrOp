45cff814d80a2bd2ba15835546354d0b6ee453b7
robertbutacu <robert.butacu@yahoo.com>
2017-12-08 20:38:32 +0200

    implicit example

--- src/Main.scala
+++ src/Main.scala
@@ -1,5 +1,7 @@
-import stringOperations.operators.Positive
+import stringOperations.operators.{Positive, StringNumber}
 
 object Main extends App{
+  implicit def doubleToPositive[T: Numeric](i: T): Positive = Positive(i.toString.span(_ != '.')._1)
   println(Positive("2", "2") + 3.4)
+  println(3.0 + Positive("1"))
 }
